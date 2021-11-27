f8cf348ca7bf6164215c327de5d9737992c1f325
Minh Phan <anhminh1993@icloud.com>
2021-11-27 14:07:16 -0800

    begin dynamic programming

--- src/seamcarving/DynamicProgrammingSeamFinder.java
+++ src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -12,7 +12,16 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
 
     @Override
     public List<Integer> findSeam(Picture picture, EnergyFunction f) {
-        // TODO: Replace with your code
+        double[][] arr = new double[picture.width()][picture.height()];
+
+        // compute the energy of the first row
+        for (int y = 0; y < picture.height(); i+=1) {
+            arr[0][y] = f.apply(picture, 0, y);
+        }
+
+        //recursion ig?
+
+
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }
