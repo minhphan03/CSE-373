3c94fbae1e8d0c2d9606eb0ed2d89e986735b41d
Minh Phan <anhminh1993@icloud.com>
2021-11-27 15:33:03 -0800

    finish implementing. testing later

--- src/seamcarving/DynamicProgrammingSeamFinder.java
+++ src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -1,5 +1,6 @@
 package seamcarving;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,16 +13,106 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
 
     @Override
     public List<Integer> findSeam(Picture picture, EnergyFunction f) {
-        double[][] arr = new double[picture.width()][picture.height()];
+        double[][] energyTable = new double[picture.width()][picture.height()];
 
-        // compute the energy of the first row
-        for (int y = 0; y < picture.height(); i+=1) {
-            arr[0][y] = f.apply(picture, 0, y);
+
+        // compute the energy of every single pixel
+        for (int x = 0; x < picture.width(); x+=1) {
+            for (int y = 0; y < picture.height(); y+=1) {
+                energyTable[x][y] = f.apply(picture, x, y);
+
+            }
+        }
+        double[][] seamDynamic = new double[picture.width()][picture.height()];
+        /**
+         * backtracker is used to track the possible paths for later tracking:
+         *  -1: first column have no previous pixels to trace
+         *  0: lower left of column y-1
+         *  1: left of current column y
+         *  2: upper left of column y+1
+         */
+        int[][] backtrace = new int[picture.width()][picture.height()];
+
+        List<Integer> seam = new ArrayList<>();
+        double min;
+
+        // mark backtracking
+        for (int x = 0; x < picture.width(); x+=1) {
+            for (int y = 0; y < picture.height(); y+=1) {
+                if (x == 0) {
+                    seamDynamic[x][y] = energyTable[x][y];
+                    backtrace[x][y] = -1;
+                } else {
+                    // for the middle columns: use the min algorithm to trace the least expensive route
+
+                    //at the bottom (y=0)
+                    if (y == 0) {
+                        min = Math.min(seamDynamic[x-1][y], seamDynamic[x-1][y+1]);
+                        // if min is the direct left one
+                        if (min == seamDynamic[x-1][y]) {
+                            backtrace[x][y] = 1;
+                        } else {
+                            backtrace[x][y] = 2;
+                        }
+                    }
+                    //if it is at the top
+                    else if (y == picture.height()-1) {
+                        min = Math.min(seamDynamic[x - 1][y], seamDynamic[x - 1][y - 1]);
+                        if (min == seamDynamic[x - 1][y]) {
+                            backtrace[x][y] = 1;
+                        } else {
+                            backtrace[x][y] = 0;
+                        }
+                    }
+                    //otherwise, check all three previous pixels
+                    else {
+                        min = Math.min(seamDynamic[x - 1][y], Math.min(seamDynamic[x - 1][y - 1], seamDynamic[x-1][y+1]));
+                        if (min == seamDynamic[x - 1][y]) {
+                            backtrace[x][y] = 1;
+                        } else if (min == seamDynamic[x - 1][y-1]) {
+                            backtrace[x][y] = 0;
+                        } else {
+                            backtrace[x][y] = 2;
+                        }
+                    }
+                    seamDynamic[x][y] = energyTable[x][y]+ min;
+                }
+
+            }
         }
 
-        //recursion ig?
+        //back-trace, starting at the end
+
+        //find min seam value
+        double min_num = seamDynamic[picture.width()-1][0];
+        int min_idx = 0;
+        for (int y = 0; y < picture.height(); y+=1) {
+            if (min_num > seamDynamic[picture.width() - 1][y]) {
+                min_idx = y;
+                min_num = seamDynamic[picture.width() - 1][y];
+            }
+        }
+
+        seam.add(min_idx);
+        int x = picture.width()-1;
+        int y = min_idx;
+
+        int backtrack;
+        while (x > 0) {
+            backtrack = backtrace[x][y];
+            if (backtrack==0) {
+                y = y-1;
+            } else if (backtrack== 2) {
+                y = y+1;
+            }
+            // add from the left
+            seam.add(0,y);
+        }
+        // first column: add itself
+        seam.add(0,y);
 
 
-        throw new UnsupportedOperationException("Not implemented yet");
+        return seam;
+        //throw new UnsupportedOperationException("Not implemented yet");
     }
 }
